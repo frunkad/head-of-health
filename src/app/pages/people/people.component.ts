015d7275ed72d8837ee454c571d11370bfbb4e7d
Darshan Baid <darshanbaid22@gmail.com>
2019-05-09 02:18:57 +0530

    follow button

--- src/app/pages/people/people.component.ts
+++ src/app/pages/people/people.component.ts
@@ -13,13 +13,18 @@ export class PeopleComponent implements OnInit {
 
   everyone: Observable<User[]>;
   user: User;
+  followings: string[];
 
   constructor(private auth: AuthService, private db: DatabaseService) {
+    this.followings = [];
     this.auth.user$.subscribe(user => {
       if (user == null) {
         this.user = null;
       } else {
         this.user = user;
+        const followings = this.db.getFollowings(user['uid']).subscribe(val => {
+          val.forEach(t => this.followings.push(t['to']));
+        });
       }
     });
   }
@@ -30,6 +35,14 @@ export class PeopleComponent implements OnInit {
   follow(uid: string) {
     console.log(this.user);
     return this.db.setFollow(this.user['uid'], uid);
+  }
+
+  doesFollow(uid: string): boolean {
+    return this.followings.includes(uid);
+  }
+
+  log(uid: string) {
+    alert(this.doesFollow(uid));
   }
 
 }
